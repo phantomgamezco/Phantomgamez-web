453c040a9ce2162bd8c880fb28bb85e25a182f25
phantomgamezco <phntomco@gmail.com>
2026-07-18 20:37:16 -0400

    Update vr-shop.js

--- vr-shop.js
+++ vr-shop.js
@@ -65,7 +65,7 @@ function handleVrPurchase(itemId, button) {
     return;
   }
 
-  const confirmed = window.confirm(`Pay ${item.price} for ${item.name} using PayPal?`);
+  const confirmed = window.confirm(`Pay ${item.price} for ${item.name} using Pay?`);
   if (!confirmed) return;
 
   purchasedVrItems.add(itemId);
